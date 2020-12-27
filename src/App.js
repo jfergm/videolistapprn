e18415d50c8b6046085985e1070826e3889e2e9e
José <jose.gomezm12@gmail.com>
2020-12-27 00:02:45 -0600

    tab nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'native-base';
 
 import { SocketIOProvider } from './context/SocketIOContext';
 
 import ListScreen from './screens/ListScreen';
 import AddItemScreen from './screens/AddItemScreen';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
     <SocketIOProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='List'>
-          <Stack.Screen name="List" component={ListScreen} />
-          <Stack.Screen name="AddItem" component={AddItemScreen}/>
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color }) => {
+              let iconName;
+              
+              switch(route.name) {
+                case 'List':
+                  iconName = 'list';
+                  break;
+                case 'AddItem':
+                  iconName = 'add-outline';
+                  break;
+              }
+  
+              // You can return any component that you like here!
+              return <Icon type="Ionicons" name={iconName} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="List" component={ListScreen} />
+          <Tab.Screen name="AddItem" component={AddItemScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </SocketIOProvider>
   )
