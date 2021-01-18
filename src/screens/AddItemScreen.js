02fe124d6e58cf2f79ae03ffa584675362a644c6
José <jose.gomezm12@gmail.com>
2021-01-18 15:45:47 -0600

    add no connected

--- src/screens/AddItemScreen.js
+++ src/screens/AddItemScreen.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
 import AddItem from '../components/AddItem';
-import { SocketIOProvider } from '../context/SocketIOContext';
+import NoConnected from '../components/NoConnected';
+import { SocketIOContext } from '../context/SocketIOContext';
 
 
 const AddItemScreen = ({ navigation }) => {
-  return(
-  <SocketIOProvider>
-    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
-        <AddItem />
-    </View>
-  </SocketIOProvider>
+  const [socket] = useContext(SocketIOContext);
 
-  );
+  if(socket) {
+    return(
+      <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+          <AddItem />
+      </View>
+    );
+  } else {
+    return(
+      <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+        <NoConnected />
+      </View>
+    )
+  }
+
 }
 
 
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,18 +1,27 @@
-import React from 'react';
-
-import { SocketIOProvider } from '../context/SocketIOContext';
+import React, { useContext } from 'react';
+import  { View } from 'react-native';
+import { SocketIOContext } from '../context/SocketIOContext';
 
 import List from '../components/List';
+import NoConnected from '../components/NoConnected';
 import { useNavigation, useTheme } from '@react-navigation/native';
 
 const ListScreen = () => {
   const { colors } = useTheme();
   const navigation = useNavigation();
-  return(
-    <SocketIOProvider>
+  const [socket] = useContext(SocketIOContext);
+
+  if(socket) {
+    return(
       <List colors={colors} navigation={navigation}></List>
-    </SocketIOProvider>
-  )
+    )
+  } else {
+    return(
+      <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+        <NoConnected />
+      </View>
+    )
+  }
 }
 
 export default ListScreen;
